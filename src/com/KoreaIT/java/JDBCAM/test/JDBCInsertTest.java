46849cc06ece9f6d916aba7b2de09635280a68a1
jihyeon2434 <ubis970420@gmail.com>
2024-01-05 16:10:44 +0900

    JDBC Insert 테스트

--- src/com/KoreaIT/java/JDBCAM/test/JDBCInsertTest.java
+++ src/com/KoreaIT/java/JDBCAM/test/JDBCInsertTest.java
@@ -2,11 +2,13 @@ package com.KoreaIT.java.JDBCAM.test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class JDBCInsertTest {
 	public static void main(String[] args) {
 		Connection conn = null;
+		PreparedStatement pstmt = null;
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -14,6 +16,20 @@ public class JDBCInsertTest {
 
 			conn = DriverManager.getConnection(url, "root", "");
 			System.out.println("연결 성공!");
+
+			String sql = "INSERT INTO article ";
+			sql += "SET regDate = NOW(),";
+			sql += "updateDate = NOW(),";
+			sql += "title = CONCAT('제목', RAND()),";
+			sql += "`body` = CONCAT('내용', RAND());";
+
+			System.out.println(sql);
+
+			pstmt = conn.prepareStatement(sql);
+
+			int affectedRow = pstmt.executeUpdate();
+
+			System.out.println("affectedRow : " + affectedRow);
 
 		} catch (ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패");
@@ -23,6 +39,13 @@ public class JDBCInsertTest {
 			try {
 				if (conn != null && !conn.isClosed()) {
 					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				if (pstmt != null && !pstmt.isClosed()) {
+					pstmt.close();
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
